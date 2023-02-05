b3b62749e57d0b5c8e697878b8d26782f24aa446
AtesB <108800814+atesbb@users.noreply.github.com>
2023-02-05 13:30:25 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import Three from './ThreeTrial/ThreeMainPage';
+
+function App() {
+  return (
+    <div className="App" style={{height: "700px", marginTop: "200px"  }}>
+      <div style={{backgroundColor: "lightblue"}}>
+      <Three></Three>
+      </div>
+
+        
+
+    </div>
+  );
+}
+
+export default App;
